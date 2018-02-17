c943836d0556e06421a9df579c0297053da9e9d6
David Arayan <david.arayan12@gmail.com>
2018-02-17 12:03:15 +1100

    - Commit the first code, none of this is working at the moment - Still trying to come up with a clean voxel interface

---
+++ EzyVoxel/Assets/Framework/VoxelChunk.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EzyVoxel {
+    public sealed class VoxelChunk {
+        public const int DIM_X = 16;
+        public const int DIM_Y = 16;
+        public const int DIM_Z = 16;
+
+        public const int VOXEL_COUNT = DIM_X * DIM_Y * DIM_Z;
+
+        private readonly short[] voxels;
+
+        public VoxelChunk() {
+            this.voxels = new short[VOXEL_COUNT];
+        }
+
+        public short this[int index] {
+            get {
+                return voxels[index];
+            }
+        }
+
+        public short this[int x, int y, int z] {
+            get {
+                return voxels[x + DIM_X * (y + DIM_Y * z)];
+            }
+        }
+    }
+}
