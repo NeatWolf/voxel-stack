a7fb6a5ec5faf383a5170a8969639a665846b4e2
David Arayan <david.arayan12@gmail.com>
2018-11-06 17:55:24 +1100

    - Added quick test code for the morton keys, to study how they work

--- EzyVoxel/Assets/Test.cs
+++ EzyVoxel/Assets/Test.cs
@@ -2,12 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using VoxelLUT;
+using BitStack;
 
 public class Test : MonoBehaviour {
 
+	public Vector3[] data;
+
 	// Use this for initialization
 	void Start () {
-        BlockLUT.Get(0);
+		data = new Vector3[new Vector3(3, 3, 3).MortonKey() + 1];
+
+		for (int i = 0; i < data.Length; i++) {
+			data[i] = Vector3.negativeInfinity;
+		}
+		for (int x = 0; x < 4; x++) {
+			for (int y = 0; y < 4; y++) {
+				for (int z = 0; z < 4; z++) {
+					Vector3 vector = new Vector3(x, y, z);
+
+					data[vector.MortonKey()] = vector;
+				}
+			}
+		}
 	}
 	
 	// Update is called once per frame
@@ -17,6 +33,6 @@ public class Test : MonoBehaviour {
 
     private void OnDrawGizmos()
     {
-        BlockLUT.Get(0).OnDebug();
+        
     }
 }
