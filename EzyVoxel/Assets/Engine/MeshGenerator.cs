49a0d6ad1b062f868cbacc931830f5eb789cc24e
David Arayan <david.arayan12@gmail.com>
2018-11-11 13:45:50 +1100

    - Re-added VoxelChunk and MeshGenerator classes - This new structure is required to move the generator code in a compute shader

---
+++ EzyVoxel/Assets/Engine/MeshGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VoxelStackLUT;
+using BitStack;
+
+namespace VoxelStack {
+	public class MeshGenerator {
+		
+		public static int FillVertices(byte voxel, uint mortonKey, ref Vector3[] array, int from) {
+			Vector3[] vertexLutTable = GeneratedVoxelTable.VertexTable;
+			int[] indexLutTable = GeneratedVoxelTable.IndexTable;
+			
+			int indexFrom = indexLutTable[voxel];
+			int indexTo = indexLutTable[voxel + 1];
+			
+			Vector3 decodedKey = mortonKey.DecodeMortonKey3();
+			
+			for (int i = indexFrom; i < indexTo; i++) {
+				array[from++] = vertexLutTable[i] + decodedKey;
+			}
+			
+			return from;
+		}
+		
+		public static int FillNormals(byte voxel, ref Vector3[] array, int from) {
+			Vector3[] vertexLutTable = GeneratedVoxelTable.NormalTable;
+			int[] indexLutTable = GeneratedVoxelTable.IndexTable;
+			
+			int indexFrom = indexLutTable[voxel];
+			int indexTo = indexLutTable[voxel + 1];
+			
+			for (int i = indexFrom; i < indexTo; i++) {
+				array[from++] = vertexLutTable[i];
+			}
+			
+			return from;
+		}
+	}
+}
---
+++ EzyVoxel/Assets/Engine/VoxelChunk.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using BitStack;
+
+namespace VoxelStack {
+
+	/**
+	 * Each Voxel Chunk has the following characteristics
+	 * - Each Chunk contains
+	 * - - 4 x 4 x 4 Voxels = 64 Voxels in Total
+	 * - - 1 x 32 bits for the morton key
+	 *
+	 * - Each Voxel contains
+	 * - - 1 x 16 bits for the Voxel Type
+	 * - - 1 x 64 bits for the Voxel States
+	 * - - 64 x 8 bits for the Voxel Neighbour States
+	 */
+	public sealed class VoxelChunk {
+		public const uint VOXEL_TYPE_BYTES = 16 / 8;
+		public const uint VOXEL_STATES_BYTES = 64 / 8;
+		public const uint VOXEL_NEIGHBOUR_BYTES = 64;
+		public const uint VOXEL_INDEX_SHIFT = VOXEL_TYPE_BYTES + VOXEL_STATES_BYTES + VOXEL_NEIGHBOUR_BYTES;
+		
+		public const uint CHUNK_SIZE = 4;
+		public const uint CHUNK_VOXELS = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;
+		
+		private byte[] state;
+		
+		public VoxelChunk() {
+			this.state = new byte[CHUNK_VOXELS * VOXEL_INDEX_SHIFT];
+		}
+		
+		public ushort this[uint x, uint y, uint z] {
+			get {
+				uint key = BitMath.EncodeMortonKey(x, y, z) * VOXEL_INDEX_SHIFT;
+				
+				return new ValueTuple<byte, byte>(state[key], state[key+1]).CombineToUShort();
+			}
+			set {
+				uint key = BitMath.EncodeMortonKey(x, y, z) * VOXEL_INDEX_SHIFT;
+				
+				var splitValues = value.SplitIntoByte();
+				
+				state[key] = splitValues.Item1;
+				state[key+1] = splitValues.Item2;
+				
+				// TO/DO
+			}
+		}
+	}
+}
