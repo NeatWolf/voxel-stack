95510cadfe44ea487f49c313dc4facc129538183
David Arayan <david.arayan12@gmail.com>
2018-11-15 09:12:54 +1100

    - resync the BitStack library - Added new Voxel structure for easier read operations from the VoxelChunk class.

--- EzyVoxel/Assets/Engine/VoxelChunk.cs
+++ EzyVoxel/Assets/Engine/VoxelChunk.cs
@@ -3,7 +3,54 @@ using System.Collections;
 using BitStack;
 
 namespace VoxelStack {
-
+	
+	/**
+	 * Represents a simple structure which encodes data
+	 * about a single voxel value. Each voxel has a single value
+	 * however contains multiple subvoxels up to 64 values.
+	 *
+	 * The state of the subvoxels is represented as an unsigned long (64 bits)
+	 */
+	public struct Voxel {
+		private readonly ushort type;
+		private readonly ulong state;
+		
+		public Voxel(ushort type, ulong state) {
+			this.type = type;
+			this.state = state;
+		}
+		
+		public ushort Type { 
+			get { 
+				return type; 
+			}
+		}
+		
+		public ulong State { 
+			get { 
+				return state; 
+			}
+		}
+		
+		public int this[int index] {
+			get {
+				return state.BitAt(index);
+			}
+		}
+		
+		public int StateCount {
+			get {
+				return state.PopCount();
+			}
+		}
+		
+		public int IsEnabled {
+			get {
+				return state > 0 ? 1 : 0;
+			}
+		}
+	}
+	
 	/**
 	 * Each Voxel Chunk has the following characteristics
 	 * - Each Chunk contains
@@ -19,32 +66,44 @@ namespace VoxelStack {
 		public const uint VOXEL_TYPE_BYTES = 16 / 8;
 		public const uint VOXEL_STATES_BYTES = 64 / 8;
 		public const uint VOXEL_NEIGHBOUR_BYTES = 64;
-		public const uint VOXEL_INDEX_SHIFT = VOXEL_TYPE_BYTES + VOXEL_STATES_BYTES + VOXEL_NEIGHBOUR_BYTES;
 		
 		public const uint CHUNK_SIZE = 4;
 		public const uint CHUNK_VOXELS = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;
 		
+		public const uint CHUNK_SHIFT = VOXEL_TYPE_BYTES * CHUNK_VOXELS;
+		
 		private byte[] state;
+		private ulong[] voxelSubstates;
 		
 		public VoxelChunk() {
-			this.state = new byte[CHUNK_VOXELS * VOXEL_INDEX_SHIFT];
+			this.state = new byte[CHUNK_VOXELS + (VOXEL_TYPE_BYTES * CHUNK_VOXELS)];
+			this.voxelSubstates = new ulong[CHUNK_VOXELS];
 		}
 		
-		public ushort this[uint x, uint y, uint z] {
+		public Voxel this[uint x, uint y, uint z] {
 			get {
-				uint key = BitMath.EncodeMortonKey(x, y, z) * VOXEL_INDEX_SHIFT;
+				uint key = BitMath.EncodeMortonKey(x, y, z) + (VOXEL_TYPE_BYTES - 1);
 				
-				return new ValueTuple<byte, byte>(state[key], state[key+1]).CombineToUShort();
+				return new Voxel(new ValueTuple<byte, byte>(state[key - 1], state[key]).CombineToUShort(), voxelSubstates[key]);
 			}
 			set {
-				uint key = BitMath.EncodeMortonKey(x, y, z) * VOXEL_INDEX_SHIFT;
+				uint key = BitMath.EncodeMortonKey(x, y, z) + (VOXEL_TYPE_BYTES - 1);
 				
-				var splitValues = value.SplitIntoByte();
+				var splitValues = value.Type.SplitIntoByte();
 				
-				state[key] = splitValues.Item1;
-				state[key+1] = splitValues.Item2;
+				state[key - 1] = splitValues.Item1;
+				state[key] = splitValues.Item2;
 				
-				// TO/DO
+				voxelSubstates[key] = value.State;
+				
+				uint mortonKeyFront = BitMath.EncodeMortonKey(x, y, z - 1);
+				uint mortonKeyBack = BitMath.EncodeMortonKey(x, y, z + 1);
+				uint mortonKeyLeft = BitMath.EncodeMortonKey(x - 1, y, z);
+				uint mortonKeyRight = BitMath.EncodeMortonKey(x + 1, y, z);
+				uint mortonKeyUp = BitMath.EncodeMortonKey(x, y + 1, z);
+				uint mortonKeyDown = BitMath.EncodeMortonKey(x, y - 1, z);
+				
+				
 			}
 		}
 	}
